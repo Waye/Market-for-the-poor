568c8bbe572811e3c2786797ee50f79fade4c439
jhma17 <jma910212@gmail.com>
2019-04-01 18:41:05 -0400

    authentication

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const MessageSchema = new mongoose.Schema({
     time: Date,
@@ -9,14 +11,70 @@ const MessageSchema = new mongoose.Schema({
 
 const UserSchema = new mongoose.Schema({
     name: String,
-    password: String,
-    email: String,
+    password:  {
+		type: String,
+		required: true,
+        minlength: 6
+    },
+    email: {
+		type: String,
+		//mongoose schema attribute required
+		required: true,
+		minlength: 3,
+		trim: true, // trim whitespace
+		unique: true,
+		validate: {
+			validator: validator.isEmail,
+			message: 'Not valid email'
+		}
+    },
+    //array of message id
     messages: [MessageSchema],
     role: String,
     status: Boolean,
+    //array of post id
     posts: [String],
     phone: String,
     description: String
 });
+
+// Our own student finding function 
+UserSchema.statics.findByEmailPassword = function(email, password) {
+	const User = this
+
+	return User.findOne({email: email}).then((user) => {
+		if (!user) {
+			return Promise.reject()
+		}
+
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(password, user.password, (error, result) => {
+				if (result) {
+					resolve(user);
+				} else {
+					reject();
+				}
+			})
+		})
+	})
+}
+
+// This function runs before saving user to database
+UserSchema.pre('save', function(next) {
+	const user = this
+
+	if (user.isModified('password')) {
+		bcrypt.genSalt(10, (error, salt) => {
+			bcrypt.hash(user.password, salt, (error, hash) => {
+				user.password = hash
+				next()
+			})
+		})
+	} else {
+		next()
+	}
+
+})
+
 
 module.exports = mongoose.model("User", UserSchema);
