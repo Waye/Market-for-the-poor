3bcfdcd0ed57ce04710c751e39a9af8c84213596
Junkai Li <leestephen75@gmail.com>
2019-04-05 00:01:00 -0400

    db

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-// const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://leeestephen:qb2Z42TWDYj34qZ@cluster0-w2ffd.mongodb.net/csc309?retryWrites=true'
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/UserAPI'
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://leeestephen:qb2Z42TWDYj34qZ@cluster0-w2ffd.mongodb.net/csc309?retryWrites=true'
+// const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/UserAPI'
 mongoose.connect(mongoURI, { useNewUrlParser: true});
 module.exports = {mongoose}
