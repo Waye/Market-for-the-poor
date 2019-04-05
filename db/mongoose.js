c249be83ae376642a7b24bc904a5e6507c3d5706
Junkai Li <leestephen75@gmail.com>
2019-04-04 23:14:28 -0400

    Change to online db

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/UserAPI'
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://leeestephen:qb2Z42TWDYj34qZ@cluster0-w2ffd.mongodb.net/csc309?retryWrites=true'
 mongoose.connect(mongoURI, { useNewUrlParser: true});
 module.exports = {mongoose}
--- public/js/nav_search.js
+++ public/js/nav_search.js
@@ -30,8 +30,12 @@ $("#topNav").on('keydown', '#searchInput', function(e){
                                 </div>
                             </div>
                         </div>`)
-                    
                     $('#searchrResultModal').modal('show');
+                    // const offset = $("#searchInput").offset();
+                    $("#searchrResultModal").css({
+                        'top': $("#searchInput").top() + $("#searchInput").height() + 20,
+                        'left': $("#searchInput").left() + 20
+                    })
                 },
                 error: function(result) {
                     if (result.status == '500') {
@@ -49,11 +53,15 @@ $("#topNav").on('keydown', '#searchInput', function(e){
 
 function getSearchResult(result) {
     let html = ''
+    let postImage = post.image;
+    if (post.image == "") {
+        postImage = "/img/post-initial-image.png";
+    }
     for (let post of result) {
         post.date = new Date(post.date).toLocaleDateString("en-US", dataFormatNav)
         
         html += `<div class="row mt-2 mb-2 border-bottom result">
-        <div class="col-3 mb-2"><img class="rounded" alt="..." src="${post.image}"></div>
+        <div class="col-3 mb-2"><img class="rounded" alt="..." src="${postImage}"></div>
         <div class="col-9">
             <h5><a href="/detail/${post._id}">${post.title}</a> <small>5 packs</small></h5>
             <p>${post.userName} posted on <span>${post.date}</span></p>
